62a225fdf504bf3539b0b9a980f7e03aa66702a2
James Boyd <james.d.boyd.iii@protonmail.com>
2022-11-25 16:08:24 -0500

    Added a hyperparameter for Dropout rate

--- toy_example.py
+++ toy_example.py
@@ -1,7 +1,12 @@
 import tensorflow as tf
+import argparse
 
 from clearml import Task
 task = Task.init(project_name='Evaluation Project', task_name='Tensorflow Training')
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dropout-rate", type=float, default=0.2)
+args = parser.parse_args()
 
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -11,7 +16,7 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
     tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dropout(args.dropout_rate),
     tf.keras.layers.Dense(10)
 ])
 
